88cd3ec90c52f223338f946a0b8a7b0a2c893ad6
Ryan Nicholls <ryannichollsdeveloper@gmail.com>
2024-02-13 22:15:25 +0000

    added new font face and smoothed out timeline transitions

--- src/javascript/timeline.js
+++ src/javascript/timeline.js
@@ -16,7 +16,7 @@ const tl = new TimelineMax();
 const timelineScene = new ScrollMagic.Scene({
     duration: 6000,
     triggerElement: about,
-    triggerHook: 0.5
+    triggerHook: 0.10
 })
 .setTween(timelineAnim)
 .addTo(controller);
@@ -35,11 +35,10 @@ const tunnelImgScene = new ScrollMagic.Scene({
 document.addEventListener('scroll', function(){
     const clientHeight = document.documentElement.clientHeight;
     const aboutSectionY = about.getBoundingClientRect().y;
-    const aboutSectionHeight = about.getBoundingClientRect().height;
+    const clientThreeQuarter = clientHeight * 0.10;
 
-    // if(clientHeight > aboutSectionY + aboutSectionHeight * 1/2) {
-    if(clientHeight > aboutSectionY) {
-        console.log('true');
+    if (clientThreeQuarter > aboutSectionY) {
+        // console.log(clientHeight);
         scrollTracker.style.animation = 'revealTimeline 0.5s linear';
         scrollTrackerBackground.style.animation = 'revealTimelineBackground 0.2s linear';
         scrollTrackerBackground.style.opacity = 0.3;
@@ -47,8 +46,8 @@ document.addEventListener('scroll', function(){
         timelineDates.style.animation = 'revealTimeline 0.5s linear';
         timelineDates.style.opacity = 1;
     }
-    else{
-        console.log('false');
+    else {
+        // console.log('false');
         scrollTracker.style.animation = 'hideTimeline 0.5s linear';
         scrollTrackerBackground.style.animation = 'hideTimelineBackground 0.5s linear';
         scrollTrackerBackground.style.opacity = 0;
