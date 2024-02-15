e7aba6031b623fc2670b03daa6aa474652a62c3f
Ryan Nicholls <ryannichollsdeveloper@gmail.com>
2024-02-15 17:17:28 +0000

    remove auto scroll

--- docs/javascript/index-javascript/carousel.js
+++ docs/javascript/index-javascript/carousel.js
@@ -20,9 +20,12 @@ prevDom.onclick = function(){
     showSlider('prev');    
 }
 let runTimeOut;
-let runNextAuto = setTimeout(() => {
-    next.click();
-}, timeAutoNext)
+
+// Uncomment for auto scroll
+// let runNextAuto = setTimeout(() => {
+//     next.click();
+// }, timeAutoNext)
+
 function showSlider(type){
     let  SliderItemsDom = SliderDom.querySelectorAll('.carousel .list .item');
     let thumbnailItemsDom = document.querySelectorAll('.carousel .thumbnail .item');
