072b8122e86e0c9232eb2491f667e002b0c9021c
Ryan Nicholls <ryannichollsdeveloper@gmail.com>
2024-02-18 22:34:03 +0000

    further js changes to animate text in on scroll and next click

--- docs/javascript/index-javascript/new-carousel.js
+++ docs/javascript/index-javascript/new-carousel.js
@@ -31,6 +31,44 @@ palmPrev.onclick = function() {
     showSlider('palm', 'prev');    
 }
 
+function setContent(){
+    chapter = document.querySelector('.carousel .list .item:nth-child(1) .chapter');
+    title = document.querySelector('.carousel .list .item:nth-child(1) .title');
+    description = document.querySelector('.carousel .list .item:nth-child(1) .description');
+    buttons = document.querySelector('.carousel .list .item:nth-child(1) .buttons');
+
+    chapter.style.transform = 'translateY(0)';
+    title.style.transform = 'translateY(0)';
+    description.style.transform = 'translateY(0)';
+    buttons.style.transform = 'translateY(0)';
+
+    chapter.style.filter = 'blur(20px)';
+    title.style.filter = 'blur(20px)';
+    description.style.filter = 'blur(20px)';
+    buttons.style.filter = 'blur(20px)';
+    
+    chapter.style.opacity = '0';
+    title.style.opacity = '0';
+    description.style.opacity = '0';
+    buttons.style.opacity = '0';
+}
+
+function animateContent(){
+    chapter.style.animation = 'showContent 0.5s 1s linear 1 forwards';
+    chapter.style.animationDelay = '0.2s';
+
+    title.style.animation = 'showContent 0.5s 1s linear 1 forwards';
+    title.style.animationDelay = '0.4s';
+    
+    description.style.animation = 'showContent 0.5s 1s linear 1 forwards';
+    description.style.animationDelay = '0.6s';
+    
+    buttons.style.animation = 'showContent 0.5s 1s linear 1 forwards';
+    buttons.style.animationDelay = '0.8s';
+}
+
+setContent();
+
 // Function to move slider to next or previous item
 function showSlider(carouselType, type) {
     let carouselSelector = `.carousel.${carouselType}`;
@@ -41,14 +79,19 @@ function showSlider(carouselType, type) {
     let thumbnailBorderDom = document.querySelector(`${carouselSelector} .thumbnail`);
     let carouselDom = document.querySelector(`${carouselSelector}`);
 
+
     if (type === 'next') {
         SliderDom.appendChild(SliderItemsDom[0]);
         thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
         carouselDom.classList.add('next');
+        setContent();
+        animateContent();
     } else {
         SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
         thumbnailBorderDom.prepend(thumbnailItemsDom[thumbnailItemsDom.length - 1]);
         carouselDom.classList.add('prev');
+        setContent();
+        animateContent();
     }
     
     // Clear previous animation class after animation duration
@@ -67,31 +110,19 @@ document.addEventListener('scroll', function(){
     const tunnelsSection = document.querySelector('.tunnel');
     const thumbnail = document.querySelector('.thumbnail');
 
-    chapter = document.querySelector('.carousel .list .item:nth-child(1) .chapter');
-    title = document.querySelector('.carousel .list .item:nth-child(1) .title');
-    description = document.querySelector('.carousel .list .item:nth-child(1) .description');
-    buttons = document.querySelector('.carousel .list .item:nth-child(1) .buttons');
-
-
     if (clientThreeQuarter > tunnelsSectionY) {
         tunnelsSection.style.opacity = 1;
-        thumbnail.style.animation = 'animation: showThumbnails 1s linear forwards';
+        thumbnail.style.animation = 'showThumbnails 1s linear forwards';
     }
 
     if (clientHeight * 0.20 > tunnelsSectionY) {
+        setContent();
+        animateContent();
         thumbnail.style.animation = 'showThumbnails 1s linear forwards';
-
-        chapter.style.animation = 'showContent 0.5s 1s linear 1 forwards';
-        chapter.style.animationDelay = '0.2s';
-
-        title.style.animation = 'showContent 0.5s 1s linear 1 forwards';
-        title.style.animationDelay = '0.4s';
-        
-        description.style.animation = 'showContent 0.5s 1s linear 1 forwards';
-        description.style.animationDelay = '0.6s';
-        
-        buttons.style.animation = 'showContent 0.5s 1s linear 1 forwards';
-        buttons.style.animationDelay = '0.8s';
     }
+
+    setTimeout(() => {
+        thumbnail.style.animation = '';
+    }, 3000);
 
 });
